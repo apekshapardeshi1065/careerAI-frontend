cbd59c46fb2a008963c7f9cb54fa60e8454a002b
Apeksha Pardeshi <apekshapardeshi1065@gmail.com>
2026-06-05 16:05:34 +0530

    update all api urls

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
+const BASE_URL = 'https://careerai-backend-2umd.onrender.com/api';
 
-const API = axios.create({ baseURL: 'https://careerai-backend-2umd.onrender.com/api' });
+const API = axios.create({ baseURL: BASE_URL });
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem('token');
